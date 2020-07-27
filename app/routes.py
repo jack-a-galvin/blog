f868e057cca7a321c5b974101ff251aaeb29911e
Jack Galvin <jack.anthony.galvin@gmail.com>
2020-07-27 23:49:02 +0100

    added temporary user posts on index

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"username": "Jack"}
-    return render_template("index.html", title="Little Blog", user=user)
+    user = {"username": "Jack"},
+    posts = [
+        {
+            "author": {"username": "Johnny"},
+            "body": "Quarantine is sooo BORING"
+        },
+        {
+            "author": {"username": "Alex"},
+            "body": "Please wear a mask guys"
+        }
+    ]
+    return render_template("index.html", title="Little Blog", user=user, posts=posts)
