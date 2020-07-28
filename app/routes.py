d89c6027d6560d659e66931043d39d2b8ebbfbe2
Jack Galvin <jack.anthony.galvin@gmail.com>
2020-07-28 23:33:36 +0100

    created login page and form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
     user = {"username": "Jack"},
     posts = [
@@ -16,3 +17,9 @@ def index():
         }
     ]
     return render_template("index.html", title="Little Blog", user=user, posts=posts)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
