fc7ca6ad1b1fb4ac884bdffced437db56dda4b7c
Jack Galvin <jack.anthony.galvin@gmail.com>
2020-08-02 23:05:38 +0100

    add flask_login module

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route("/")
 @app.route("/index")
